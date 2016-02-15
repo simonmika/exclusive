6ed7b127a342a0e6e38fae0fa84b577f649b5eed
Molham Khalil <molham.khalil@vidhance.com>
2016-02-15 12:29:45 +0100

    problem with Cancel button fixed

--- client/source/UI/Settings.ts
+++ client/source/UI/Settings.ts
@@ -5,24 +5,24 @@ module Imint.Exclusive.Client.UI {
 			this.Title = "Settings";
 		}
 		Setup() {
-			var serverFieldValue: any;  
-			var serverField = new Wappli.Field("server", "text", "Server", () => this.service.Server, value => this.service.Server = value);
-			this.Append(serverField);
-			var userNameFieldValue: any;
-			var userNameField = new Wappli.Field("userName", "text", "User Name", () => this.service.User, value => this.service.User = value); 
-			this.Append(userNameField);
-			var passwordFieldValue: any;
-			var passwordField = new Wappli.Field("password", "password", "Password", () => this.service.Password, value => this.service.Password = value); 
-			this.Append(passwordField);
+			var serverFieldValue = this.service.Server;
+			var userNameFieldValue = this.service.User;
+			var passwordFieldValue = this.service.Password;
+			var serverField: any;
+			var userNameField: any;
+			var passwordField: any;
+			this.Append(serverField = new Wappli.Field("server", "text", "Server", () => this.service.Server, value => this.service.Server = value));
+			this.Append(userNameField = new Wappli.Field("userName", "text", "User Name", () => this.service.User, value => this.service.User = value));
+			this.Append(passwordField = new Wappli.Field("password", "password", "Password", () => this.service.Password, value => this.service.Password = value));
 			this.Append(new Wappli.Button("Log in", () => {
 				this.service.SaveSettings();
 				window.location.href = this.service.Server + "/app/";
 			}));
 			this.Append(new Wappli.Button("Cancel", () => {
 				$.mobile.changePage(this.service.Server + "/app/", "slide");
-				serverField.Value = this.service.Server;
-				userNameField.Value = this.service.User;
-				passwordField.Value = this.service.Password;
+				serverField.Value = serverFieldValue;
+				userNameField.Value = userNameFieldValue;
+				passwordField.Value = passwordFieldValue;
 			}));
 		}
 	}
