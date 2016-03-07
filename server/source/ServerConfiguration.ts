12f6f01a2b1661fdaeb234a3043e15b6e5bc4837
Jonathan Udd <jonathan.udd@vidhance.com>
2016-03-07 15:27:46 +0100

    Command line argument one will now be used to find the config file

--- server/source/ServerConfiguration.ts
+++ server/source/ServerConfiguration.ts
@@ -7,9 +7,9 @@ module Exclusive {
 		export var AppPath: string;
 		export var AuthorisationServer: string;
 		export var AuthorisationPath: string;
-		export function ReadServerConfigurations(): void {
+		export function ReadServerConfigurations(configFile: string): void {
 			try {
-				var configuration = JSON.parse(fs.readFileSync('config.json', 'utf-8'));
+				var configuration = JSON.parse(fs.readFileSync(configFile, 'utf-8'));
 				ServerConfiguration.HostName = configuration.hostName;
 				ServerConfiguration.Port = configuration.port;
 				ServerConfiguration.Protocol = configuration.protocol;
