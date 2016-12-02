7d86a72c4b5750c1115745acf06229eb0b682c67
Simon Mika <simon@mika.se>
2016-12-02 15:03:45 +0100

    Starts without a working configuration.

--- server/source/ServerConfiguration.ts
+++ server/source/ServerConfiguration.ts
@@ -8,21 +8,23 @@ module Exclusive {
 		export var AuthorisationServer: string;
 		export var AuthorisationPath: string;
 		export function ReadServerConfigurations(configFile: string): void {
+			var configuration: any;
 			try {
-				var configuration = JSON.parse(fs.readFileSync(configFile, 'utf-8'));
-				ServerConfiguration.HostName = configuration.hostName;
-				ServerConfiguration.Port = configuration.port;
-				ServerConfiguration.Protocol = configuration.protocol;
-				ServerConfiguration.DataLocalPath = path.resolve(configuration.data, '');
-				var absolutePath = path.resolve(configuration.build, '');
-				ServerConfiguration.AppPath = path.join(absolutePath, 'app');
-				ServerConfiguration.AuthorisationServer = configuration.authorisationServer;
-				ServerConfiguration.AuthorisationPath = configuration.authorisationPath;
+				configuration = JSON.parse(fs.readFileSync(configFile, 'utf-8'));
 			}
 			catch (error) {
-				console.log("There was an error while reading the configuration file, unable to continue.\n" + error.toString());
-				process.exit(1);
+				console.error('WARNING: There was an error while reading the configuration file, falling back to defaults.\n' + error.toString());
 			}
+			if (!configuration)
+				configuration = {}
+			ServerConfiguration.HostName = configuration.hostName ? configuration.hostName : 'localhost';
+			ServerConfiguration.Port = configuration.port ? configuration.port : 8080;
+			ServerConfiguration.Protocol = configuration.protocol ? configuration.protocol : 'http';
+			ServerConfiguration.DataLocalPath = path.resolve(configuration.data ? configuration.data : 'data', '');
+			var absolutePath = path.resolve(configuration.build ? configuration.build : 'build', '');
+			ServerConfiguration.AppPath = path.join(absolutePath, 'app');
+			ServerConfiguration.AuthorisationServer = configuration.authorisationServer;
+			ServerConfiguration.AuthorisationPath = configuration.authorisationPath;
 		}
 	}
 }
